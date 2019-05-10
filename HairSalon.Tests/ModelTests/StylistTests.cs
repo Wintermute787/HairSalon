cba9bc7d9a19c8e246f9f071f93ddf815c0d3b17
Joe Barnes <joebarnes333@gmail.com>
2019-05-10 16:53:11 -0700

    add readme info

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -52,6 +52,54 @@ namespace HairSalon.Tests
 
             CollectionAssert.AreEqual(testList, result);
         }
+  [TestMethod]
+        public void Save_DatabaseAssignsIdToStylist_Id()
+        {
+           
+            Stylist testStylist = new Stylist("name",1);
+            testStylist.Save();
+
+            
+            Stylist savedStylist = Stylist.GetAll()[0];
+
+            int result = savedStylist.GetId();
+            int testId = testStylist.GetId();
+
+           
+            Assert.AreEqual(testId, result);
+        }
+
+        [TestMethod]
+        public void Find_FindsStylistInDatabase_Stylist()
+        {
+            
+            Stylist testStylist = new Stylist("name",1);
+            testStylist.Save();
+
+            
+            Stylist foundStylist = Stylist.Find(testStylist.GetId());
+
+           
+            Assert.AreEqual(testStylist, foundStylist);
+        }
+
+        [TestMethod]
+        public void GetClients_RetrievesAllClientsWithStylists_ClientList()
+        {
+            
+         
+            Stylist testStylist = new Stylist("name",1);
+            testStylist.Save();
+            Client client01 = new Client("joe", testStylist.GetId());
+            client01.Save();
+            Client client02 = new Client("barnes", testStylist.GetId());
+            client02.Save();
+            List<Client> testClientList = new List<Client> {client01, client02};
+            List<Client> resultClientList = testStylist.GetClients();
+
+            //Assert
+            CollectionAssert.AreEqual(testClientList, resultClientList);
+        }
 
     }
 }
---
+++ readme.md
@@ -0,0 +1,44 @@
+# _Joe Barnes_
+
+#### _Web app for a Hair Salon, 05/10/19 _
+
+#### By _**Joe Barnes**_
+
+## Description
+
+_A hair salon owner may create Stylists in the database and save clients to them. _
+
+## Setup/Installation Requirements
+
+* _Clone from github_
+* _run dotnet restore in root directory_
+* _use dotnet run to launch server_
+* _navigate to localhost:5000 to view_
+
+
+_Launch mysql in console. 
+CREATE DATABASE()
+CREATE DATABASE hairSalon
+SHOW DATABASE
+USE hairSalon
+SELECT DATABASE();
+CREATE TABLE stylists ( id serial PRIMARY KEY, name VARCHAR (255))
+CREATE TABLE clients (id serial PRIMARY KEY, name VARCHAR (255) stylistId INT)
+DROP DATABASE hairSalon
+_
+
+## Known Bugs
+
+_Currently there are no Known bugs_
+
+## Support and contact details
+
+_If there any any bugs contact me at joebaranes333@gmail.com_
+
+## Technologies Used
+
+_C#, HTML, MYSQL, _
+
+### License
+
+Copyright (c) 2019 **_Joe Barnes_**
