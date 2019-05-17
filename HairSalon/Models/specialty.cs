22a3c95ddd0fb0b686298d5b9f4078ce3381d667
Joe Barnes <joebarnes333@gmail.com>
2019-05-17 10:08:06 -0700

    completed logic for specialty

--- HairSalon/Models/specialty.cs
+++ HairSalon/Models/specialty.cs
@@ -60,6 +60,115 @@ namespace HairSalon.Models
             conn.Open();
             MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
             cmd.CommandText = @"DELETE FROM specialties;";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+
         }
+
+        public static Specialty Find(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;  
+            cmd.CommandText = @"SELECT * FROM specialties WHERE id = @thisId;";
+            MySqlParameter thisId = new MySqlParameter();
+            thisId.ParameterName = "@thisId";
+            thisId.Value = id;
+            cmd.Parameters.Add(thisId);
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            int specialtyId = 0;
+            string specialtyDescription = "";
+            while(rdr.Read())
+            {
+                specialtyId = rdr.GetInt32(0);
+                specialtyDescription = rdr.GetString(1);
+            }
+            Specialty newSpecialty = new Specialty(specialtyId, specialtyDescription);
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+            return newSpecialty;
+        }
+
+        public override bool Equals(System.Object otherSpecialty)
+        {
+            if(!(otherSpecialty is Specialty))
+            {
+                return false;
+            }
+            else
+            {
+                Specialty newSpecialty = (Specialty) otherSpecialty;
+                bool idEquality = (this.GetId()) == newSpecialty.GetId();
+                bool descriptionEquality = (this.GetDescription()) == newSpecialty.GetDescription();
+                return (idEquality && descriptionEquality);        
+            }
+        }
+
+        public void Edit(string newDescription)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"UPDATE specialties SET description = @newDescription WHERE id = @searchId;";
+            MySqlParameter searchId = new MySqlParameter(); 
+            searchId.ParameterName = "@searchId";
+            searchId.Value = _id;
+            cmd.Parameters.Add(searchId);
+            MySqlParameter description = new MySqlParameter();
+            description.ParameterName = "@newDescription";
+            description.Value = newDescription;
+            cmd.Parameters.Add(description);
+            cmd.ExecuteNonQuery();
+            _description = newDescription;
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO specialties (description) VALUES (@description);";
+            MySqlParameter description = new MySqlParameter();
+            description.ParameterName = "@description";
+            description.Value = this._description;
+            cmd.Parameters.Add(description);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Delete()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM specialties WHERE id = @searchId;";
+            MySqlParameter searchId = new MySqlParameter();
+            searchId.ParameterName = "@searchId";
+            searchId.Value = _id;
+            cmd.Parameters.Add(searchId);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
     }
 }
